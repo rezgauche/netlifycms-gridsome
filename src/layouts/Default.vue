525a8c8fdc55a294a5ebaa369643a41665d4e685
rezgauche <94529557+rezgauche@users.noreply.github.com>
2021-11-21 13:45:58 +0100

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -16,8 +16,8 @@
     </main>
 
     <footer class="footer">
-      <span class="footer__copyright">Copyright © {{ new Date().getFullYear() }}. </span>
-      <span class="footer__links">Powered by <a href="//www.suits.at"> SUITS </a></span>
+      <span class="footer__copyright">Copyside © {{ new Date().getFullYear() }}. </span>
+      //<span class="footer__links">Powered by <a href="//www.suits.at"> SUITS </a></span>
     </footer>
 
   </div>
