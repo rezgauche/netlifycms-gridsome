80cd341e6810fb7fc4bf6b2136f47cafc8746ddb
rezgauche <94529557+rezgauche@users.noreply.github.com>
2021-11-23 18:19:40 +0100

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,24 +1,15 @@
 <template>
 	<div class="author">
-		
-		<h1>
-		Welcome to
-    		<strong class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-      		{{$static.metadata.siteName}}
-    		</strong>
-  		</h1>
 
 		<h1 v-if="showTitle" class="author__site-title">
 			{{ $static.metadata.siteName }}
 		</h1>
 
 		<p class="author__intro">
-			rezgauche.be is responsive to a need for an immediate representation of artists, balancing responsibility with decentralised organisation.
+			artworks & more
 		</p>
 
 		<p class="author__links">
-			<a href="https://www.3xr.space/store/rezgauche.mintbase1.near">gallery</a>
-			<a href="https://www.instagram.com/rezgauche/">insta</a>
 			<a href="https://reverent-noether-6cff94.netlify.app/item-01">01</a>
 		</p>
 
